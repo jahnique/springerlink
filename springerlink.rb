d6986f7875653b40ae876f2111ee78d957019900
Yannic <yannic.schencking@zweitag.de>
2013-11-18 03:30:48 +0100

    initial commit

---
+++ springerlink.rb
@@ -0,0 +1,50 @@
+#!/usr/bin/env ruby
+#UTF-8 encoding
+
+require 'nokogiri'
+require 'open-uri'
+
+class Springerlink
+  def initialize(journal_id)
+    page_numbers = get_total_page_number(journal_id)
+    page_numbers.times do |page_no|
+      doc = get_document(journal_id, page_no+1)
+      article_hash = get_article_hash(doc)
+      download_articles(article_hash, doc)
+    end
+  rescue
+    puts "\nNo journal ID specified.\n\nExample: springerlink.rb 11576"
+  end
+
+  def get_total_page_number(journal_id)
+    get_document(journal_id, 1).css('span.number-of-pages').first.text.to_i
+  end
+
+  def get_document(journal_id, page_number)
+    Nokogiri::HTML(open("http://link.springer.com/search/page/#{page_number}?facet-journal-id=#{journal_id}&sortOrder=newestFirst&facet-content-type=Article"))
+  end
+
+  def get_article_hash(document)
+    published_dates = document.css('span.year')
+    article_hash = {}
+    document.css('a.title').map { |article| article_hash[article['href']] = article.text.gsub(/[\x00\/\\:\*\?\"<>\|„“]/, '') }
+    article_hash.keys.map.with_index { |article,i| article_hash[["http://link.springer.com/content/pdf/", article.delete("/article").sub(/s/, "%2Fs")]] = [ published_dates[i].attributes["title"].value.split.reverse, article_hash.delete(article) ] }
+    article_hash
+  end
+
+  def download_articles(article_hash, document)
+    article_hash.each do |link|
+      journal = document.css('img.cover').first.attributes["alt"].value
+      prefix = link[0][0]
+      article = link[0][1]
+      title = link[1][1]
+      year = link[1][0][0]
+      month = Date::MONTHNAMES.index(link[1][0][1])
+      `mkdir -p #{journal}/#{year}/#{month}`
+      `wget #{prefix+article}`
+      `mv #{article} '#{journal}/#{year}/#{month}/#{title}.pdf'`
+    end
+  end
+end
+
+Springerlink.new(ARGV[0])
