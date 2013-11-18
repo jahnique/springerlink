d4cf4438da9ba4b185b7580ea6f1376f26dfc01c
Yannic <yannic.schencking@zweitag.de>
2013-11-18 04:54:38 +0100

    adds better crawl mechanism

--- springerlink.rb
+++ springerlink.rb
@@ -12,8 +12,8 @@ class Springerlink
       article_hash = get_article_hash(doc)
       download_articles(article_hash, doc)
     end
-  rescue
-    puts "\nNo journal ID specified.\n\nExample: springerlink.rb 11576"
+  #rescue
+    #puts "\nNo journal ID specified.\n\nExample: springerlink.rb 11576"
   end
 
   def get_total_page_number(journal_id)
@@ -26,23 +26,23 @@ class Springerlink
 
   def get_article_hash(document)
     published_dates = document.css('span.year')
+    download_links = []
+    document.css('a.pdf-link').map { |link| download_links << "http://link.springer.com" + link['href'] }
     article_hash = {}
-    document.css('a.title').map { |article| article_hash[article['href']] = article.text.gsub(/[\x00\/\\:\*\?\"<>\|„“]/, '') }
-    article_hash.keys.map.with_index { |article,i| article_hash[["http://link.springer.com/content/pdf/", article.delete("/article").sub(/s/, "%2Fs")]] = [ published_dates[i].attributes["title"].value.split.reverse, article_hash.delete(article) ] }
+    document.css('a.title').map.with_index { |article,i| article_hash[download_links[i]] = article.text.gsub(/[\x00\/\\:\*\?\"<>\|„“]/, '') }
+    article_hash.keys.map.with_index { |article,i| article_hash[article] = [ published_dates[i].attributes["title"].value.split.reverse, article_hash.delete(article) ] }
     article_hash
   end
 
   def download_articles(article_hash, document)
     article_hash.each do |link|
       journal = document.css('img.cover').first.attributes["alt"].value
-      prefix = link[0][0]
-      article = link[0][1]
+      download_link = link[0][0]
       title = link[1][1]
       year = link[1][0][0]
       month = Date::MONTHNAMES.index(link[1][0][1])
       `mkdir -p #{journal}/#{year}/#{month}`
-      `wget #{prefix+article}`
-      `mv #{article} '#{journal}/#{year}/#{month}/#{title}.pdf'`
+      `wget #{download_link} -O '#{journal}/#{year}/#{month}/#{title}.pdf'`
     end
   end
 end
